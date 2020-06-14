b61a53f0a9d673ae06dccc2d0e0f57a7461873eb
jpc0rrea <joaopedrocorrea14@gmail.com>
2020-06-14 11:35:32 -0300

    Add SessionController

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const app = express();
 
-app.post("/users", (req, res) => {
-  return res.json({ message: "Hello João" });
-});
+mongoose.connect(
+  "mongodb+srv://omnistack1:omnistack1@aircnc-th3ni.mongodb.net/aircnc?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
