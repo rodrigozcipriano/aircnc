97a5dad0c3aebef64bacfb538784e080dff7dde7
rodrigozcipriano <rodrigozcipriano1@gmail.com>
2020-06-17 19:46:22 -0300

    Adding frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require('./routes');
+const cors = require("cors");
+
+const routes = require("./routes");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@aircnc-9tcgr.mongodb.net/aircnc?retryWrites=true&w=majority',{
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@aircnc-9tcgr.mongodb.net/aircnc?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     userUnifiedTopology: true,
-})
+  }
+);
 
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 app.listen(3333);
-
-
